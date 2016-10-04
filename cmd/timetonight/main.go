04b1353636040e17a96fe8af11287086c475ae5e
Brian Lachniet <blachniet@gmail.com>
2016-10-03 20:32:38 -0400

    Add sample index

--- cmd/timetonight/main.go
+++ cmd/timetonight/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,69 +8,25 @@ import (
 
 	"github.com/blachniet/timetonight"
 	"github.com/blachniet/timetonight/mock"
-	"gopkg.in/dougEfresh/gtoggl.v8"
+	"github.com/blachniet/timetonight/toggl"
 )
 
 func main() {
-	targetDuration := 9 * time.Hour
-	togglToken := os.Getenv("TIMETON_TOGGLTOKEN")
-	if togglToken == "" {
-		log.Fatalf("No Toggl API token found in env variable TIMETON_TOGGLTOKEN")
-	}
+	togglToken := os.Getenv("TOGGL_API_TOKEN")
 
-	tc, err := gtoggl.NewClient(togglToken)
-	if err != nil {
-		log.Fatalf("Failed to connect to Toggl: %v", err)
-	}
-
-	entries, err := tc.TimeentryClient.List()
-	if err != nil {
-		log.Fatalf("Err getting Toggl time entries: %v", err)
-	}
-
-	var todayDur time.Duration
-	var timerRunning bool
-	for _, entry := range entries {
-		nowYear, nowMonth, nowDay := time.Now().Local().Date()
-		startYear, startMonth, startDay := entry.Start.Local().Date()
-		if nowYear == startYear && nowMonth == startMonth && nowDay == startDay {
-			if entry.Duration > 0 {
-				todayDur += time.Duration(entry.Duration) * time.Second
-			} else {
-				timerRunning = true
-				todayDur += (time.Duration(time.Now().UTC().Unix()) * time.Second) + (time.Duration(entry.Duration) * time.Second)
-			}
-		}
-	}
-
-	fmt.Print("Timer    :")
-	if timerRunning {
-		fmt.Println(" On")
-	} else {
-		fmt.Println(" Off")
-	}
-
-	hours := todayDur / time.Hour
-	minutes := (todayDur - (hours * time.Hour)) / time.Minute
-	fmt.Printf("Logged   : %dh %dm\n", hours, minutes)
-
-	remainingDur := targetDuration - todayDur
-	if remainingDur > 0 {
-		remHours := remainingDur / time.Hour
-		remMin := (remainingDur - (remHours * time.Hour)) / time.Minute
-		fmt.Printf("Remaining: %dh %dm\n", remHours, remMin)
-
-		if timerRunning {
-			fmt.Printf("=== You should be done around %v\n", time.Now().Local().Add(remainingDur))
-		} else {
-			fmt.Printf("=== If you start your timer now you should be done around %v\n", time.Now().Local().Add(remainingDur))
-		}
-	} else {
-		fmt.Println("=== You're done!")
-	}
-
-	timer := &mock.Timer{}
 	persister := &mock.Persister{}
+	persister.TogglAPITokenFn = func() (string, error) {
+		return togglToken, nil
+	}
+	persister.TimePerDayFn = func() (time.Duration, error) {
+		return time.Hour * 9, nil
+	}
+
+	timer, err := toggl.NewTimer(togglToken)
+	if err != nil {
+		log.Fatalf("Err creating Toggl timer: %+v", err)
+	}
+
 	ren, err := timetonight.NewDefaultRenderer("./templates/*.tmpl", true)
 	if err != nil {
 		log.Fatalf("Err creating renderer: %+v", err)
@@ -89,5 +44,33 @@ func main() {
 
 func getIndex(t timetonight.Timer, p timetonight.Persister, ren timetonight.Renderer,
 	w http.ResponseWriter, r *http.Request) (int, error) {
-	return http.StatusOK, ren.Render(w, "index.tmpl", nil)
+
+	timePerDay, err := p.TimePerDay()
+	if err != nil {
+		log.Printf("Failed to retrieve time per day: %+v", err)
+		return http.StatusInternalServerError, err
+	}
+
+	durToday, err := t.LoggedToday()
+	if err != nil {
+		log.Printf("Failed to retrieve time logged today: %+v", err)
+		return http.StatusInternalServerError, err
+	}
+
+	running, err := t.IsRunning()
+	if err != nil {
+		log.Printf("Failed to retrieve whether timer is running: %+v", err)
+		return http.StatusInternalServerError, err
+	}
+
+	hours := durToday / time.Hour
+	minutes := (durToday - (hours * time.Hour)) / time.Minute
+	data := struct {
+		HoursPerDay   int
+		LoggedHours   int
+		LoggedMinutes int
+		TimerRunning  bool
+	}{int(timePerDay / time.Hour), int(hours), int(minutes), running}
+
+	return http.StatusOK, ren.Render(w, "index.tmpl", data)
 }
